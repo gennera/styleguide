cebcfd9d6be21060d57bb9246b4ad68c4acd2b31
Fernando Machado Bernardino <fernandombernardino@gmail.com>
2017-04-06 19:16:29 -0300

    add sass to gulp

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+const root = 'client/admin';
+
+// ----------------------------------------
+// Sass
+// ----------------------------------------
+gulp.task('sass', () => {
+  return gulp.src([
+    './scss/style.scss',
+    './scss/overrides/bootstrap/bootstrap-override.scss'
+  ])
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
